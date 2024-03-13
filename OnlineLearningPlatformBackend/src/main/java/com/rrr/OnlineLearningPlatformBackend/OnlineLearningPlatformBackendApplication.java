bc3b66173120a8c67e84f613f381bfb79fd2b892
ramana5130 <venkataramanareddygopu@gmail.com>
2024-03-13 20:21:00 +0530

    first commit

---
+++ OnlineLearningPlatformBackend/src/main/java/com/rrr/OnlineLearningPlatformBackend/OnlineLearningPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rrr.OnlineLearningPlatformBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLearningPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLearningPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ OnlineLearningPlatformBackend/src/test/java/com/rrr/OnlineLearningPlatformBackend/OnlineLearningPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrr.OnlineLearningPlatformBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
